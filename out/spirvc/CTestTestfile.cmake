89443ab508cf233ed5d8191cf61f74141b54255b
http80 <70071615+http-80@users.noreply.github.com>
2025-03-24 21:52:13 +0100

    remove garbage

--- out/spirvc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mana25/genesis/Native/spirvc
-# Build directory: C:/mana25/genesis/Native/out/spirvc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/spirvc/gitversion.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2021 The Khronos Group Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-#ifndef SPIRV_CROSS_GIT_VERSION_H_
-#define SPIRV_CROSS_GIT_VERSION_H_
-
-#define SPIRV_CROSS_GIT_REVISION "Git commit: vulkan-sdk-1.4.304.0-40-g2c32b6bf Timestamp: 2025-02-18T16:07:36"
-
-#endif
